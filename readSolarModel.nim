400a8365ac58f919ba7166e33742da09109fcfb1
Johanna von Oy <jojovoy@outlook.de>
2020-01-15 14:16:02 +0100

    only read solar model if  run as main

--- readSolarModel.nim
+++ readSolarModel.nim
@@ -6,16 +6,17 @@ proc readSolarModel(fname: string): DataFrame =
   ## later use a `Table[string, seq[float]]` instead of the dataframe!
   result = toDf(readCsv(fname, sep = ' '))
 
-const solarModel = "./ReadSolarModel/resources/AGSS09_solar_model_stripped.dat"
+when isMainModule:
+  const solarModel = "./ReadSolarModel/resources/AGSS09_solar_model_stripped.dat"
 
-let df = readSolarModel(solarModel)
-echo df.pretty(precision = 10)
+  let df = readSolarModel(solarModel)
+  echo df.pretty(precision = 10)
 
-# to read a single column, e.g. radius:
-echo df["Radius"]
+  # to read a single column, e.g. radius:
+  echo df["Radius"]
 
-# now let's plot radius against temperature colored by density
-ggplot(df, aes("Radius", "Temp", color = "Rho")) +
-  geom_line() +
-  ggtitle("Radius versus temperature of solar mode, colored by density") +
-  ggsave("radius_temp_density.pdf")
+  # now let's plot radius against temperature colored by density
+  ggplot(df, aes("Radius", "Temp", color = "Rho")) +
+    geom_line() +
+    ggtitle("Radius versus temperature of solar mode, colored by density") +
+    ggsave("radius_temp_density.pdf")
