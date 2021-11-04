b38a4e94cfb00bb5b86d18b4faba7666c7ab4d99
Vindaar <basti90@gmail.com>
2021-11-04 11:04:19 +0100

    replace #steps ↦ #points, auto split request into parts if more >500

--- tools/download_henke_files.nim
+++ tools/download_henke_files.nim
@@ -1,4 +1,5 @@
-import std / [httpclient, strutils, os, parseutils, strformat]
+import std / [httpclient, strutils, os, parseutils, strformat, math]
+import seqmath
 
 const henkeUrl = "https://henke.lbl.gov/"
 const materialInterface = "cgi-bin/laymir.pl"
@@ -28,21 +29,21 @@ type
     polUnpolarized = 0
     polS = 1
   HenkeReq = object
-    layer: string                     # chemical formula of layer
-    layerDensity: float               # layer density in g/cm³ (if -1: tabulated value density)
-    layerThickness: float             # thickness of layer in nm
-    surfaceRoughness: float           # top surface roughness in nm ("sigma1")
-    substrateMaterial: string         # chemical formula of the substrate material
-    substrateDensity: float           # density of the substrate material (if -1: tab. value density)
-    substrateRoughness: float         # top surface roughness in nm of substrate ("sigma2")
-    polarization: PolarizationKind    # polarization (s polarization: 1, p polarization: -1, unpol.: 0)
-    scanQuantity: ScanKind            # quantity to scan over
-    scanMin: range[0.0 .. 30000.0]    # minimum value from which to scan
-    scanMax: range[0.0 .. 30000.0]    # minimum value from which to scan
-    numberOfPoints: range[0 .. 499]   # number of scan points (max 499)
-    fixedQuantity: FixedKind          # quantity to keep fixed
-    fixedValue: range[0.0 .. 30000.0] # fixed value of the quantity
-    plotKind: PlotKind                # kind of plot to produce
+    layer: string                        # chemical formula of layer
+    layerDensity: float                  # layer density in g/cm³ (if -1: tabulated value density)
+    layerThickness: float                # thickness of layer in nm
+    surfaceRoughness: float              # top surface roughness in nm ("sigma1")
+    substrateMaterial: string            # chemical formula of the substrate material
+    substrateDensity: float              # density of the substrate material (if -1: tab. value density)
+    substrateRoughness: float            # top surface roughness in nm of substrate ("sigma2")
+    polarization: PolarizationKind       # polarization (s polarization: 1, p polarization: -1, unpol.: 0)
+    scanQuantity: ScanKind               # quantity to scan over
+    scanMin: range[0.0 .. 30000.0]       # minimum value from which to scan
+    scanMax: range[0.0 .. 30000.0]       # minimum value from which to scan
+    numberOfPoints: range[0 .. int.high] # number of scan points (max 500; if more we split the download into multiple parts)
+    fixedQuantity: FixedKind             # quantity to keep fixed
+    fixedValue: range[0.0 .. 30000.0]    # fixed value of the quantity
+    plotKind: PlotKind                   # kind of plot to produce
     outputKind: OutputKind
 
 proc toStr(f: float): string =
@@ -63,7 +64,7 @@ proc `$`(h: HenkeReq): string =
   result.add "Scan=" & $h.scanQuantity & "&"
   result.add "Min=" & $h.scanMin & "&"
   result.add "Max=" & $h.scanMax & "&"
-  result.add "Npts=" & $h.numberOfPoints & "&"
+  result.add "Npts=" & $(h.numberOfPoints - 1) & "&" # henke takes number of *steps* and not data points
   result.add "temp=" & $h.fixedQuantity & "&"
   result.add "Fixed=" & $h.fixedValue & "&"
   result.add "Plot=" & $h.plotKind & "&"
@@ -80,7 +81,7 @@ proc initHenkeReq(layer = "Au",
                    scanQuantity = skEnergy,
                    scanMin = 30.0,
                    scanMax = 5000.0,
-                   numberOfPoints = 499,
+                   numberOfPoints = 500,
                    fixedQuantity = fkAngle,
                    fixedValue = 1.0,
                    plotKind = pkLinear,
@@ -129,31 +130,56 @@ proc extractDataPath(resp: string): string =
       let num = l.parseUntil(result, '\"', findIt.len + idx)
       break
 
-proc downloadDatafile(client: HttpClient, file: string): string =
-  ## Downloads the data file from the Henke server
-  result = client.getContent(henkeUrl & file)
-  var res = result.splitLines
+proc downloadDatafile(client: HttpClient, file: string): seq[string] =
+  ## Downloads the data file from the Henke server and returns is split by lines
+  ## for easier handling of the header.
+  var data = client.getContent(henkeUrl & file)
+  result = data.strip.splitLines
   var idx = 0
-  for i, l in mpairs(res):
+  for i, l in mpairs(result):
     if i > 1: break
     if i == 0: l = "# " & l.strip
     else: l = "#" & l.strip.replace(" ", "").replace(",", " ")
-  var l0 = res[0]
-  res[0] = res[1]
-  res[1] = l0
-  result = res.join("\n")
+  var l0 = result[0]
+  result[0] = result[1]
+  result[1] = l0
 
-proc storeDatafile(data: string, h: HenkeReq): string =
+proc storeDatafile(data: seq[string], h: HenkeReq) =
   let angle = &"{h.fixedValue.float:.2f}"
   let thickness = &"{h.layerThickness / 1000.0:.2f}"
   let outfile = outpath / fileTmpl % [angle, thickness]
   createDir(outpath)
-  writeFile(outfile, data.strip & "\n")
+  let dataStr = data.join("\n")
+  writeFile(outfile, dataStr.strip & "\n")
+
+proc download(client: HttpClient, h: HenkeReq) =
+  ## Takes care of downloading (all) the file(s) for the given Henke request.
+  ## This means if there are more than 500 data points requested, it will be
+  ## split into multiple data files
+  const maxPoints = 500
+  let scanVals = linspace(h.scanMin, h.scanMax, h.numberOfPoints) # all values to scan
+  let nchunks = ceilDiv(h.numberOfPoints, maxPoints)
+  var data: seq[string]
+  var pointsLeft = h.numberOfPoints
+  for chunk in 0 ..< nchunks:
+    var hl = h
+    hl.numberOfPoints = min(pointsLeft, maxPoints) # remaining points or max points
+    hl.scanMin = scanVals[chunk * maxPoints]       # get min and max from values
+    hl.scanMax = scanVals[min((chunk + 1) * maxPoints, h.numberOfPoints) - 1]
+    let dataPath = client.postHenke(hl).extractDataPath()
+    let chkData = client.downloadDatafile(dataPath)
+    if data.len == 0: # simply add all data including header
+      data.add chkData
+    else:             # only add data without header
+      data.add chkData[2 ..< chkData.len]
+    if chunk < nchunks - 1: # in last chunk cannot decrease, as `pointsLeft` is a positive range
+      dec pointsLeft, maxPoints
+  storeDatafile(data, h)
 
 let client = newHttpClient()
-let req = initHenkeReq()
+let req = initHenkeReq(numberOfPoints = 511)
 
-echo client.downloadDatafile(extractDataPath(extr)).storeDatafile(req)
+client.download(req)
 
 when false:
   import datamancer
