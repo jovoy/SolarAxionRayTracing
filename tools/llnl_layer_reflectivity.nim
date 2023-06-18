825d520059e8edd831b0ff8d5111305857f12c57
Vindaar <basti90@gmail.com>
2023-06-18 15:34:16 +0200

    [tools] update LLNL reflectivity tool to generate custom ranges
    This allows to run the code with the ranges required by our code or
    for REST.
    
    To generate the file for our code, run without arguments. For REST run
    via
    
    ```sh
    ./llnl_layer_reflectivity \
        --numEnergy 500 \
        --angleMax 9.0.° \
        --numAngle 901 \
        --outfile llnl_layer_reflectivities_rest.h5 \
        --outpath /path/to/foo
    ```
    
    This still needs to be converted to raw binary files.

--- tools/llnl_layer_reflectivity.nim
+++ tools/llnl_layer_reflectivity.nim
@@ -1,7 +1,27 @@
-import nimpy, ggplotnim, nimhdf5
+import nimpy, ggplotnim, nimhdf5, unchained
 import scinim / numpyarrays
+from std / os import `/`
 
-#let np = pyImport("numpy")
+#[
+Simple tool to generate an HDF5 file for the reflectivities of all the
+different layers of the LLNL telescope.
+
+It requires the Python library `darpanx`.
+
+Run the script without arguments to generate the file that we need for us.
+
+To generate the file as required by REST, run instead:
+
+```sh
+./llnl_layer_reflectivity \
+    --numEnergy 500 \
+    --angleMax 9.0.° \
+    --numAngle 901 \
+    --outfile llnl_layer_reflectivities_rest.h5 \
+    --outpath /path/to/foo
+```
+]#
+
 let drp = pyImport("darpanx")
 
 let m1 = drp.Multilayer(MultilayerType="DepthGraded",
@@ -44,37 +64,50 @@ let m4 = drp.Multilayer(MultilayerType="DepthGraded",
                         Repetition=5,
                         SigmaValues=[1.0])
 
-
 let ms = [m1, m2, m3, m4]
 
-let Energy = linspace(0.03, 15.0, 1000) # scan in a range wider than we use in the raytracer
-let θs = linspace(0.0, 1.5, 1000) # [0.5] # the angle under which we check
+proc main(energyMin = 0.03.keV,
+          energyMax = 15.0.keV,
+          numEnergy = 1000,
+          angleMin = 0.0.°,
+          angleMax = 1.5.°,
+          numAngle = 1000,
+          outfile = "llnl_layer_reflectivities.h5",
+          outpath = "../resources/") =
+  let Energy = linspace(energyMin.float, energyMax.float, numEnergy) # scan in a range wider than we use in the raytracer
+  let θs = linspace(angleMin.float, angleMax.float, numAngle) # [0.5] # the angle under which we check
 
-var h5f = H5open("../resources/llnl_layer_reflectivities.h5", "rw")
-var eDset = h5f.create_dataset("Energy", (Energy.len, 1), dtype = float64)
-var aDset = h5f.create_dataset("Angles", (θs.len, 1), dtype = float64)
-eDset[eDset.all] = Energy
-aDset[aDset.all] = θs
+  echo Energy
+  var h5f = H5open(outpath / outfile, "rw")
+  var eDset = h5f.create_dataset("Energy", (Energy.len, 1), dtype = float64)
+  var aDset = h5f.create_dataset("Angles", (θs.len, 1), dtype = float64)
+  eDset[eDset.all] = Energy
+  aDset[aDset.all] = θs
 
-let Enp = Energy.toTensor.toNdArray
-for i, m in ms:
-  var df = newDataFrame()
-  var refl = zeros[float]([Energy.len, θs.len])
-  var ϑidx = 0
-  for ϑ in θs:
-    # compute the reflectivity
-    discard m.get_optical_func(Theta = [ϑ], Energy = Enp)
-    let reflect = toTensor[float64](m.Ra)
-    refl[ϑidx, _] = reflect
-    let dfLoc = seqsToDf({"Energy" : Energy, "Theta" : ϑ, "Ref" : reflect})
-    df.add dfLoc
-    inc ϑidx
-  echo df
-  #df.writeCsv("/tmp/reflectivity_angles.csv")
-  ggplot(df, aes("Energy", "Theta", fill = "Ref")) +
-    geom_raster() +
-    ggtitle("Reflectivity of LLNL layer type " & $i) +
-    ggsave("/tmp/layer" & $i & ".pdf")
-  var rDset = h5f.create_dataset("Reflectivity" & $i, (Energy.len, θs.len),
-                                 dtype = float64)
-  rDset.unsafeWrite(refl.dataArray, refl.size)
+  let Enp = Energy.toTensor.toNdArray
+  for i, m in ms:
+    var df = newDataFrame()
+    var refl = zeros[float]([θs.len, Energy.len])
+    var ϑidx = 0
+    for ϑ in θs:
+      # compute the reflectivity
+      discard m.get_optical_func(Theta = [ϑ], Energy = Enp)
+      let reflect = toTensor[float64](m.Ra)
+      refl[ϑidx, _] = reflect.unsqueeze(axis = 0)
+      let dfLoc = seqsToDf({"Energy" : Energy, "Theta" : ϑ, "Ref" : reflect})
+      df.add dfLoc
+      inc ϑidx
+    echo df
+    #df.writeCsv("/tmp/reflectivity_angles.csv")
+    ggplot(df, aes("Energy", "Theta", fill = "Ref")) +
+      geom_raster() +
+      ggtitle("Reflectivity of LLNL layer type " & $i) +
+      ggsave("/tmp/layer" & $i & ".pdf")
+    var rDset = h5f.create_dataset("Reflectivity" & $i, (θs.len, Energy.len),
+                                   dtype = float64)
+    rDset.unsafeWrite(refl.dataArray, refl.size)
+
+when isMainModule:
+  import cligen
+  import unchained / cligenParseUnits
+  dispatch main
